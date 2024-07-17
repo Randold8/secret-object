b3f42c953df302baee0a552b95367a3fb35b19e7
Randold8 <84187343+Randold8@users.noreply.github.com>
2024-07-17 23:16:25 +0300

    kuplinov addition

---
+++ kuplinov/turn.js
@@ -0,0 +1,16 @@
+let guy = document.getElementById('kuplahead');
+
+document.addEventListener('mousemove', (event) => {
+  let rect = guy.getBoundingClientRect();
+  let centerX = rect.left + rect.width/2;
+  let centerY = rect.top + rect.height/2;
+
+  let mouseX = event.clientX;
+  let mouseY = event.clientY;
+
+  let radians = Math.atan2(mouseX - centerX, mouseY - centerY);
+  let degree = radians * (180 / Math.PI) * -1 + 90;
+
+  guy.style.transform = `rotate(${degree}deg)`;
+});
+
---
+++ kuplinov/videos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      document.getElementById('latest-video').classList.add('animate');
+    }, 100);
+  });
+  
